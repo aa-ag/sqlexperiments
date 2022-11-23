7e2473b806dd45b7540fc5863904e8d87891eea3
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-23 17:03:09 -0600

    Add create table logic

--- create.sql
+++ create.sql
@@ -1,1 +1,11 @@
-CREATE DATABASE testdb OWNER aaronaguerrevere ENCODING UTF8;
+-- CREATE DATABASE testdb OWNER aaronaguerrevere ENCODING UTF8;
+CREATE TABLE testtable(
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    phone TEXT,
+    email TEXT,
+    address TEXT,
+    postalZip TEXT,
+    region TEXT,
+    country TEXT
+);
