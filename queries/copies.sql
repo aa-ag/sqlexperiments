b1a65cfafb751a13d39aa4ab287375fa0c1d4b54
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-24 06:43:53 -0600

    List columns' names from testcopy table

--- queries/copies.sql
+++ queries/copies.sql
@@ -1,3 +1,7 @@
 SELECT table_name,table_catalog FROM information_schema.tables WHERE table_schema='public';
 
 CREATE TABLE testcopy AS TABLE testtable;
+
+SELECT * FROM information_schema.columns
+WHERE table_schema = 'testdb'
+AND table_name = 'testcopy';
