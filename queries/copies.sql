6a35b5842a35b2ee23afb7af0d35043c0e31562e
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-24 06:37:15 -0600

    Query to get tables first

--- queries/copies.sql
+++ queries/copies.sql
@@ -1,7 +1,1 @@
--- https://stackoverflow.com/questions/10705616/table-name-as-a-postgresql-function-parameter
-CREATE PROCEDURE copy_tables()
-LANGUAGE SQL
-AS $$
-    CREATE TABLE x_backup AS
-    TABLE x_table;
-$$;
+SELECT * FROM information_schema.tables;
