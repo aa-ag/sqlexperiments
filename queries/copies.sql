03961c1b56961024d7dd00e4da930890c0fedd33
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-24 06:48:14 -0600

    Add expanded display + column display psql commands

--- queries/copies.sql
+++ queries/copies.sql
@@ -2,6 +2,6 @@ SELECT table_name,table_catalog FROM information_schema.tables WHERE table_schem
 
 CREATE TABLE testcopy AS TABLE testtable;
 
-SELECT * FROM information_schema.columns
-WHERE table_schema = 'testdb'
-AND table_name = 'testcopy';
+\x
+
+\d+ testcopy
