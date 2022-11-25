d70d1f129837d45c3fa393168cb1583bd775c6ef
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-25 10:38:23 -0600

    Avoiding duplicates

--- queries/pour.sql
+++ queries/pour.sql
@@ -1,5 +1,26 @@
-COPY testtable(name,phone,email,address,postalZip,region,country)
+-- COPY testtable(name,phone,email,address,postalZip,region,country)
+-- FROM '/Users/aaronaguerrevere/Documents/projects/sqlexperiments/fake_data_for_testing.csv' 
+-- DELIMITER ','
+-- ENCODING 'UTF8'
+-- CSV HEADER;
+
+CREATE TEMP TABLE temporary_table(
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    phone TEXT,
+    email TEXT, 
+    address TEXT,
+    postalZip TEXT,
+    region TEXT,
+    country TEXT
+);
+
+COPY temporary_table(name,phone,email,address,postalZip,region,country)
 FROM '/Users/aaronaguerrevere/Documents/projects/sqlexperiments/fake_data_for_testing.csv' 
 DELIMITER ','
-ENCODING 'UTF8'
 CSV HEADER;
+
+INSERT INTO testtable(id,name,phone,email,address,postalZip,region,country)
+SELECT *
+FROM temporary_table
+ON CONFLICT DO NOTHING;
