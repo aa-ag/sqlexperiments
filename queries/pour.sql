ee1ea2cad45df1b717a14b09d3bf26b532d5f8b3
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-25 09:32:41 -0600

    Add ENCODING to COPY

--- queries/pour.sql
+++ queries/pour.sql
@@ -1,4 +1,4 @@
 COPY testtable(name,phone,email,address,postalZip,region,country)
 FROM '/Users/aaronaguerrevere/Documents/projects/sqlexperiments/fake_data_for_testing.csv'
 DELIMITER ','
-CSV HEADER;
+CSV ENCODING 'UTF8' HEADER;
