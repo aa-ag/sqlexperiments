5e4c876bbd5f1ba6dcf01bda93dc682ef2f0c741
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-25 10:13:28 -0600

    Revert to copy but add encoding

--- queries/pour.sql
+++ queries/pour.sql
@@ -1,7 +1,5 @@
--- first, duplicate a table, without any source data
-CREATE TABLE testcopy
-AS TABLE testtable
-WITH NO DATA;
--- second, create a temporary table with the original source data
-COPY testcopy(name,phone,email,address,postalZip,region,country)
-FROM '/Users/aaronaguerrevere/Documents/projects/sqlexperiments/fake_data_for_testing.csv'
+COPY testtable(name,phone,email,address,postalZip,region,country)
+FROM '/Users/aaronaguerrevere/Documents/projects/sqlexperiments/fake_data_for_testing.csv' 
+DELIMITER ','
+ENCODING 'UTF8'
+CSV HEADER;
