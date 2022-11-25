d456114194c83fa4887a1775af627da037e5cacd
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-25 09:45:45 -0600

    Copy in memory

--- queries/pour.sql
+++ queries/pour.sql
@@ -2,3 +2,6 @@
 CREATE TABLE testcopy
 AS TABLE testtable
 WITH NO DATA;
+-- second, create a temporary table with the original source data
+COPY testcopy(name,phone,email,address,postalZip,region,country)
+FROM '/Users/aaronaguerrevere/Documents/projects/sqlexperiments/fake_data_for_testing.csv'
