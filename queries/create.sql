cc94bf40e1e07fe1a71de35f0f3b62aa8a8aa8fb
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-26 19:59:50 -0600

    Add IF NOT EXISTS to create testtable query

--- queries/create.sql
+++ queries/create.sql
@@ -1,9 +1,9 @@
 -- CREATE DATABASE testdb OWNER aaronaguerrevere ENCODING UTF8;
-CREATE TABLE testtable(
+CREATE TABLE IF NOT EXISTS testtable(
     id SERIAL PRIMARY KEY,
     name TEXT,
     phone TEXT,
-    email TEXT,
+    email TEXT, 
     address TEXT,
     postalZip TEXT,
     region TEXT,
