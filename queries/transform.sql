2ee1496506bec417980d8f787414e61d9da5958a
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-26 20:10:20 -0600

    Add CREATE of teststaged + rename copies to copy.sql for consitency

---
+++ queries/transform.sql
@@ -0,0 +1,11 @@
+-- CREATE DATABASE testdb OWNER aaronaguerrevere ENCODING UTF8;
+CREATE TABLE IF NOT EXISTS teststaged(
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    phone TEXT,
+    email TEXT, 
+    address TEXT,
+    postalZip TEXT,
+    region TEXT,
+    country TEXT
+);
