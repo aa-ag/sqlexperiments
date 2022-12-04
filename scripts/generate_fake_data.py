1de24ee07971392f6f435756e46d6aeb3cde79e7
aa-ag <aaron.aguerrevere@gmail.com>
2022-12-04 17:35:20 -0600

    Basic functionality to generate a pseudo custom field to account managers

--- scripts/generate_fake_data.py
+++ scripts/generate_fake_data.py
@@ -1,4 +1,14 @@
 from faker import Faker
 fake = Faker()
 
-print(fake.name())
+account_managers = {}
+
+i = 0
+while i < 500:
+    account_manager = {}
+    account_manager["id"] = 12345
+    account_manager["value"] = fake.name()
+    account_managers.update(account_manager)
+    i += 1
+
+print(account_managers)
