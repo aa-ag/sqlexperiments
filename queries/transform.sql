3ee9014e53195950c94faa22c5086f5e5cad0dd1
aa-ag <aaron.aguerrevere@gmail.com>
2022-12-04 17:12:53 -0600

    Add SUBSTR & SUM functions

--- queries/transform.sql
+++ queries/transform.sql
@@ -99,3 +99,13 @@ WHERE id IN (
     WHERE a.id > b.id
     AND a.email=b.email
 );
+
+SELECT
+    SUBSTR(trans_date,1,7) as month,
+    country,
+    COUNT(*) as trans_count,
+    SUM(CASE WHEN state="approved" THEN 1 ELSE 0 END) as approved_count,
+    SUM(amount) as trans_total_amount,
+    SUM(CASE WHEN state="approved" THEN amount ELSE 0 END) as approved_total_amount
+FROM Transactions
+GROUP BY month,country;
