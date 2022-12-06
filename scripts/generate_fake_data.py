344402b0da659f250cf24a4c544bd919b69b29a1
aa-ag <aaron.aguerrevere@gmail.com>
2022-12-05 18:58:41 -0600

    Generating two custom fields per user

--- scripts/generate_fake_data.py
+++ scripts/generate_fake_data.py
@@ -2,17 +2,24 @@ from faker import Faker
 fake = Faker()
 
 
-def make_account_managers():
-    account_managers = list()
+def make_fake_custom_fields():
     i = 0
+    custom_fields = list()
     while i < 501:
         account_manager = dict()
         account_manager["id"] = 12345
         account_manager["value"] = fake.name()
-        account_managers.append(account_manager)
+
+        custom_fields.append(account_manager)
+
+        cc = dict()
+        cc["id"] = 6789
+        cc["value"] = fake.credit_card_full()
+
+        custom_fields.append(cc)
+
         i += 1
+    print(custom_fields)
 
-    print(account_managers)
 
-
-make_account_managers()
+make_fake_custom_fields()
